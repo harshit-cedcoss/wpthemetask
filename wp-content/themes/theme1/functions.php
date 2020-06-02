2813c66d45345595fa0464a3ed27fb57308b189d
Harshit-Cedcoss <harshitagarwal@cedcoss.com>
2020-06-02 13:21:11 +0530

    adding theme support commit

--- wp-content/themes/theme1/functions.php
+++ wp-content/themes/theme1/functions.php
@@ -34,30 +34,139 @@ function register_my_menus() {
 			'extra-menue' => __( 'Extra Menu' ),
 		)
 	);
-	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 50, 50, array( 'center', 'center' ) ); // 50 pixels wide by 50 pixels tall, crop from the center
-	add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
-	the_post_thumbnail( 'category-thumb' );
+}
+add_action( 'init', 'register_my_menus' );
+
+/**
+ * Sidebar support.
+ */
+function my_register_sidebars() {
+	/* Register the 'primary' sidebar. */
+	register_sidebar(
+		array(
+			'id'            => 'primary',
+			'name'          => __( 'Primary Sidebar' ),
+			'description'   => __( 'A short description of the sidebar.' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+	/* Repeat register_sidebar() code for additional sidebars. */
+}
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function theme1_support() {
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Custom background color.
 	$args = array(
 		'default-color' => '0000ff',
 		'default-image' => get_template_directory_uri() . '/images/wapuu.jpg',
 	);
 	add_theme_support( 'custom-background', $args );
-}
-add_action( 'init', 'register_my_menus' );
-/**
- * Theme1 function for registering menus
- */
-function themename_custom_header_setup() {
-	$args = array(
-		'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
-		'default-text-color' => '000',
-		'width'              => 1000,
-		'height'             => 250,
-		'flex-width'         => true,
-		'flex-height'        => true,
+
+	// Set content-width.
+	global $content_width;
+	if ( ! isset( $content_width ) ) {
+		$content_width = 580;
+	}
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// Set post thumbnail size.
+	set_post_thumbnail_size( 1200, 9999, array( 'center', 'center' ) ); // 50 pixels wide by 50 pixels tall, crop from the center
+
+	// Add custom image size used in Cover Template.
+	add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
+
+	the_post_thumbnail( 'category-thumb' );
+
+	// Custom logo.
+	$logo_width  = 120;
+	$logo_height = 90;
+
+	// If the retina setting is active, double the recommended width and height.
+	if ( get_theme_mod( 'retina_logo', false ) ) {
+		$logo_width  = floor( $logo_width * 2 );
+		$logo_height = floor( $logo_height * 2 );
+	}
+
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => $logo_height,
+			'width'       => $logo_width,
+			'flex-height' => true,
+			'flex-width'  => true,
+		)
 	);
-	add_theme_support( 'custom-header', $args );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+
+	/**
+	* Editor style support
+	*/
+	add_theme_support( 'editor-styles' );
+
+	/**
+	* Post formats.
+	*/
+	add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
+
+	/**
+	 * Custom header.
+	 */
+	add_theme_support(
+		'custom-header',
+		array(
+			'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+			'default-text-color' => '000',
+			'width'              => 1000,
+			'height'             => 250,
+			'flex-width'         => true,
+			'flex-height'        => true,
+		)
+	);
+
 }
-add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+add_action( 'after_setup_theme', 'theme1_support' );
 
